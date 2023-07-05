556f7df94a227e99263dac9e4053dea8ef3d64c0
Tombal Nicolas <nicolas.tombal01@gmail.com>
2023-07-05 15:57:40 +0200

    url fix

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -8,9 +8,9 @@ import NotFound from "../components/error/NotFound";
 const Router = () => {
 	return (
 		<Routes>
-			<Route path="/" element={<Home />} />
-			<Route path="/logement/:id" element={<Rent />} />
-			<Route path="/about" element={<About />} />
+			<Route path="/Kasa" element={<Home />} />
+			<Route path="/Kasa/logement/:id" element={<Rent />} />
+			<Route path="/Kasa/about" element={<About />} />
 			<Route path="*" element={<NotFound />} />
 		</Routes>
 	);
